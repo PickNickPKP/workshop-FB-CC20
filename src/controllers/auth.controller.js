1703adfabb166da1c9272126977538daaa0a78f0
PickNickPKP <pakkapong.b6106973@gmail.com>
2025-06-29 22:34:07 +0700

    Stage deletion of check-Identity.util.js and other changes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,83 +1,12 @@
 import prisma from "../config/prisma.config.js";
-import checkIdentity from "../utils/check-Identity.util.js";
 import createError from "../utils/create.error.util.js";
-import bcrypt from "bcryptjs";
 
 export async function register(req, res, next) {
   try {
-    const { identity, firstName, lastName, password, confirmPassword } = req.body;
-    //validation
-    if (
-      !(
-        identity.trim() &&
-        firstName.trim() &&
-        lastName.trim() &&
-        password.trim() &&
-        confirmPassword.trim()
-      )
-    ) {
-      createError(400, "Please fill all data");
-    }
-    if (password !== confirmPassword) {
-      createError(400, "check confirm password");
-    }
-    //// identity เป็น email หรือ mobile phone number : vheckIdentity(identity) => String : 'email' | 'mobile'
-    const identityKey = checkIdentity(identity);
-
-    // หา User   //findUniqe เร็วกว่า findFirst
-    const foundUser = await prisma.user.findUnique({
-      where: { [identityKey]: identity },
-    });
-    console.log(foundUser);
-    if (foundUser) {
-      createError(409, `Already have this user: ${identity}`);
-    }
-
-    const newUser = {
-      [identityKey]: identity,
-      password: await bcrypt.hash(password, 10),
-      firstName: firstName,
-      lastName: lastName,
-    };
-
-    // const result = await prisma.user.create({ data: newUser });
-    res.json({
-      msg: "Register controller",
-      result: newUser,
-    });
+    res.json({ msg: "Register controller", body: req.body });
   } catch (error) {
-    next(error);
+    console.log(error);
   }
-}
-
-export async function registerYup(req, res,next) {
-	console.log(req.body)
-	try {
-		const {email, mobile, firstName, lastName, password} = req.body
-		// หา user
-		if(email) {
-			// let  foundUserEmail = await prisma.user.findUnique({where : {email : email}})
-			let  foundUserEmail = await getUserBy('email', email)
-			if(foundUserEmail) createError(409, `Email : ${email}  already register`)
-		}
-		if(mobile) {
-			let  foundUserMobile = await getUserBy('mobile', mobile)
-			if(foundUserMobile) createError(409, `Mobile : ${mobile}  already register`)
-		}
-		const newUser = {
-			email,
-			mobile,
-			password: await bcrypt.hash(password, 10),
-			firstName,
-			lastName
-		}
-		const result = await prisma.user.create({data : newUser})
-		await createUser(newUser)
-		
-		res.json({message: 'Register successful',result})
-	}catch(err) {
-		next(err)
-	}
 }
 
 export const login = (req, res, next) => {
